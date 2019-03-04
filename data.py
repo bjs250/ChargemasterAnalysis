c39edd34b59b725f67ac57635eb0930d533ae563
Brian Schendt <brianschendt@gmail.com>
2019-03-04 13:27:21 -0500

    some data files

--- data.py
+++ data.py
@@ -1,5 +1,28 @@
 import pandas as pd
 
+drugs = ["Atorvastatin Calcium",
+"Levothyroxine",
+"Lisinopril",
+"Omeprazole",
+"Metformin HCL",
+"Amlodipine/Benazepril",
+"Simvastatin",
+"Hydrocodone/Acetaminophen",
+"Metoprolol Succinate",
+"Losartan Potassiu"]
 
-if __name__ = "main":
-    print("test")
+if __name__ == "__main__":
+    #drugName = "ACETAMINOPHEN"
+    drugName = "Lisinopril".upper()
+    dataFileName = "data/" + "California Pacific.xlsx"
+
+    df = pd.read_excel(dataFileName,dtype=str)
+    df = df.fillna('')
+
+    # Search the entire file for rows that contain the drugname
+    for columnName in df.columns:
+        df[columnName] = df[columnName].apply(lambda x: x.upper())
+        searchResults = df[df[columnName].str.match(drugName)]
+        if searchResults.empty == False:
+            print(searchResults)
+    
