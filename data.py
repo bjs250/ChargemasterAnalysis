ab589f8dbcefa18bcd80b6a42b707a0479f4dccf
Brian Schendt <brianschendt@gmail.com>
2019-03-06 09:21:43 -0500

    Primitive Delivery Parsing

--- data.py
+++ data.py
@@ -6,12 +6,14 @@ if __name__ == "__main__":
     drugNames = pd.read_excel("Drugs.xlsx",dtype=str)['Drug Name'].tolist()
     dataFileNames = [f for f in listdir("data/") if isfile(join("data/", f))]
     
-    dataFileName = "data/" + dataFileNames[2]
+    rawDataFileName = dataFileNames[0].split(".")[0]
+    dataFileName = "data/" + rawDataFileName + ".xlsx"
     df = pd.read_excel(dataFileName,dtype=str)
     df = df.fillna('')
 
     # Iterate through the list of drugs
-    for drugName in drugNames[0:10]:
+    print(dataFileName)
+    for drugName in drugNames[0:8]:
         drugName = drugName.lstrip().rstrip().upper()
         drugNameSet = list()
         
@@ -29,5 +31,31 @@ if __name__ == "__main__":
                 df[columnName] = df[columnName].apply(lambda x: x.upper())
                 searchResults = df[df[columnName].str.match(drugName)]
                 if searchResults.empty == False:
-                    print(searchResults)
-    
+                    for index,result in searchResults.iterrows():
+                        hospitalName = dataFileName.split("/")[1]
+                        price = result["Price"]
+                        
+                        # Parse out delivery
+                        description = result["Description"]
+                        if "TAB" in description:
+                            delivery = "TABLET"
+                        elif "CAPSULE" or " CAP " in description:
+                            delivery = "CAPSULE"
+                        elif "ELIXIR" in description:
+                            delivery = "ELIXIR"
+                        elif "SUPPOSITORY" in description:
+                            delivery = "SUPPOSITORY"
+                        elif "INTRAVENOUS" in description:
+                            delivery = "INTRAVENOUS"
+                        elif "SUSPENSION" in description:
+                            delivery = "SUSPENSION"
+                        elif "SOLUTION" in description:
+                            delivery = "SOLUTION"
+                        else: 
+                            delivery = None
+                            print(description)
+
+                        # Parse out Dosage
+                        #print(description)
+                        print(drugName,rawDataFileName,price,delivery)
+                        
